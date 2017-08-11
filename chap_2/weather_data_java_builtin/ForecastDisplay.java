b7916dd3391e75f9ee6c0060da5692000be05ea5
lywang <lywang@tiara.sinica.edu.tw>
2017-08-11 13:09:37 +0800

    Add chap2

---
+++ chap_2/weather_data_java_builtin/ForecastDisplay.java
@@ -0,0 +1,28 @@
+import java.util.Observable;
+import java.util.Observer;
+
+public class ForecastDisplay implements Observer, DisplayElement{
+	Observable observable;
+	private float currentPressure = 29.92f;
+	private float lastPressure;
+
+	public ForecastDisplay(Observable observable){
+		this.observable = observable;
+		observable.addObserver(this);
+	}
+
+	public void update(Observable obs, Object arg){
+		if (obs instanceof WeatherData){
+			WeatherData weatherData = (WeatherData) obs;
+			lastPressure = currentPressure;
+			currentPressure = weatherData.getPressure();
+		}
+		display();
+	}
+
+
+	public void display(){
+		System.out.println("current pressure is " + currentPressure + " last pressure "+ lastPressure);
+	}
+
+}
---
+++ chap_2/weather_data_java_builtin/Subject.java
@@ -0,0 +1,5 @@
+public interface Subject{
+	public void registerObserver(Observer O);
+	public void removeObserver(Observer O);
+	public void notifyObservers();
+}
