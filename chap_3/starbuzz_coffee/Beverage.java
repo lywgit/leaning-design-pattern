97901352ae78b5a5fd49d196fadf56e0e0aed969
lywang <lywang@tiara.sinica.edu.tw>
2017-08-17 14:45:49 +0800

    Add chap 3

---
+++ chap_3/starbuzz_coffee/Beverage.java
@@ -0,0 +1,8 @@
+public abstract class Beverage{
+	String description = "Unknown Beverage";
+	public abstract double cost();
+	public String getDescription(){
+		return description;
+	}
+
+}
---
+++ chap_3/starbuzz_coffee/DarkRoast.java
@@ -0,0 +1,8 @@
+public class DarkRoast extends Beverage{
+	public DarkRoast(){
+		description = "DarkRoast";
+	}
+	public double cost(){
+		return 0.99;
+	}
+}
