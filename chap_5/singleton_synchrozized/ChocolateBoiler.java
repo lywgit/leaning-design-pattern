f1bfac191fc3c87a28990a4496b61e694e5519f0
lywang <lywang@tiara.sinica.edu.tw>
2017-09-10 12:08:11 +0800

    Add chap_5

---
+++ chap_5/singleton_synchrozized/ChocolateBoiler.java
@@ -0,0 +1,45 @@
+public class ChocolateBoiler{
+	private boolean empty;
+	private boolean boiled;
+	// singleton pattern
+	private static volatile ChocolateBoiler uniqueInstance;
+
+	private ChocolateBoiler(){
+		empty = true;
+		boiled = false;	
+	}
+	public static synchrozized ChocolateBoiler getInstance(){
+		if (uniqueInstance == null){
+			uniqueInstance = new ChocolateBoiler();
+		}
+		return uniqueInstance;
+	}
+
+
+	public void fill(){
+		if (isEmpty()){
+			empty = false;
+			boiled =false; 
+		}
+	}
+	public void drain(){
+		if (!isEmpty() && isBoiled()){
+			empty = true;
+		}
+	}
+
+	public void boil(){
+		if (!isEmpty() && !isBoiled()){
+			boiled = true;
+		}
+	}
+
+	public boolean isEmpty(){
+		return empty;
+	}
+
+	public boolean isBoiled(){
+		return boiled;
+	}
+
+}
