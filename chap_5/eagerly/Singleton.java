ccd6810f163f99535475bcb683c5e9e1aefc93e2
lywang <lywang@tiara.sinica.edu.tw>
2017-09-10 18:23:39 +0800

    Complete file

--- chap_5/eagerly/Singleton.java
+++ chap_5/eagerly/Singleton.java
@@ -1,1 +1,12 @@
-public class Singleton
+public class Singleton{
+	private static Singleton uniqueInstance = new Singleton();
+
+	private Singleton(){};
+
+	public static Singleton getInstance(){
+		return uniqueInstance;
+	}
+
+
+	}
+}
